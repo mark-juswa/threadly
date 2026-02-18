f89ab16b71ffbb8fadc2f09e5f778e48f42da463
Mark <lapolkilo@gmail.com>
2026-02-18 20:01:58 +0800

    change logo

--- frontend/src/components/auth/LoginView.jsx
+++ frontend/src/components/auth/LoginView.jsx
@@ -70,9 +70,8 @@ const LoginView = () => {
       <div className="max-w-md w-full bg-[#1c1c1c] p-8 rounded-2xl border border-gray-800 shadow-2xl text-center">
         {/* Icon */}
         <div className="flex items-center justify-center w-16 h-16 mx-auto mb-6 shadow-lg bg-gradient-to-br from-green-500 to-emerald-700 rounded-xl shadow-green-500/20">
-          <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-          </svg>
+          <img src="/Logo3.png" className="w-8 h-8 text-white"  />
+          
         </div>
 
         {/* Title */}
